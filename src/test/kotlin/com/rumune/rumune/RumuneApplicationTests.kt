506bf5cd2d4e620b562f094ebbef53aad3a23404
bhyunnie <dev.bhyunnie@gmail.com>
2024-01-24 05:04:27 +0900

    이니셜 커밋

---
+++ src/test/kotlin/com/rumune/rumune/RumuneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rumune.rumune
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RumuneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
