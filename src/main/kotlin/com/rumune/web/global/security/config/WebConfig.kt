8a86329a57724c9973c1e650f8979bb4f7c9e2d3
bhyunnie <dev.bhyunnie@gmail.com>
2024-04-20 15:45:42 +0900

    [FIX] 인터셉터 삭제

--- src/main/kotlin/com/rumune/web/global/security/config/WebConfig.kt
+++ src/main/kotlin/com/rumune/web/global/security/config/WebConfig.kt
@@ -1,14 +1,11 @@
 package com.rumune.web.global.security.config
 
-import com.rumune.web.global.security.Interceptor.JwtInterceptor
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig(
-    private val jwtInterceptor: JwtInterceptor
 ): WebMvcConfigurer {
     @Override
     override fun addCorsMappings(registry: CorsRegistry) {
@@ -20,9 +17,4 @@ class WebConfig(
             .allowCredentials(true)
             .maxAge(3600)
     }
-//    @Override
-//    override fun addInterceptors(registry: InterceptorRegistry) {
-//        registry.addInterceptor(jwtInterceptor)
-//            .addPathPatterns("/api/v1/**")
-//    }
 }
