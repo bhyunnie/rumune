331fc59d2785dba7bd9bf243b3d967d60559bd93
bhyunnie <dev.bhyunnie@gmail.com>
2024-04-21 00:10:13 +0900

    [FIX] API 문서 추가

--- src/main/kotlin/com/rumune/web/domain/user/entity/User.kt
+++ src/main/kotlin/com/rumune/web/domain/user/entity/User.kt
@@ -1,6 +1,7 @@
 package com.rumune.web.domain.user.entity
 
 import com.rumune.web.domain.common.dto.BaseEntity
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
@@ -23,27 +24,35 @@ class User (
     @Id
     @Column(name = "id", unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "사용자 ID", nullable = false, example = "0")
     val id: Long = 0,
 
     @Column(name = "email", unique = true, nullable = false)
+    @Schema(description = "이메일", nullable = false, example = "test@rumune.co.kr")
     val email: String,
 
     @Column(name = "password", nullable = false)
+    @Schema(description = "패스워드", nullable = false, example = "****")
     val pwd: String,
 
     @Column(name = "provider", nullable = false)
+    @Schema(description = "외부 서비스 종류 (가입경로)", nullable = false, example = "naver")
     val provider: String,
 
     @Column(name = "provider_id", nullable = true)
+    @Schema(description = "외부 서비스에 등록된 사용자 ID", nullable = false, example= "1512519270192")
     val providerId: String? = null,
 
     @Column(name = "name", nullable = false)
+    @Schema(description = "사용자 이름(본명)", nullable = false, example = "김개똥")
     val name: String,
 
     @Column(name="profile_image", nullable = true)
+    @Schema(description = "프로필 사진", nullable = true, example="https://example-image.png")
     val profileImage: String? = null,
 
     @Column(name="deprecated", nullable = false)
+    @Schema(description = "정지 여부", nullable = false, example="false")
     val deprecated: Boolean = false,
 
     @OneToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
--- src/main/kotlin/com/rumune/web/global/doc/OpenAPIConfig.kt
+++ src/main/kotlin/com/rumune/web/global/doc/OpenAPIConfig.kt
@@ -1,7 +1,9 @@
 package com.rumune.web.global.doc
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -13,6 +15,9 @@ class OpenAPIConfig {
             .version("v1.0.0")
             .title("RUMUNE API DOCUMENTATION")
             .description("RUMUNE API 문서입니다.")
-        return OpenAPI().info(info)
+        val components = Components().addSecuritySchemes(
+            "bearer-token", SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+        )
+        return OpenAPI().info(info).components(components)
     }
 }
