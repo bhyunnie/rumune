1810234137283d3a7f179d073349f0072e5db0d9
Lee JiMin <ad4674@gmail.com>
2024-04-29 03:54:45 +0900

    [Refactoring] 필요없는 if문 삭제, 들여쓰기 수정

--- src/main/kotlin/com/rumune/web/domain/cart/application/CartService.kt
+++ src/main/kotlin/com/rumune/web/domain/cart/application/CartService.kt
@@ -1,8 +1,6 @@
 package com.rumune.web.domain.cart.application
 
 import com.amazonaws.services.kms.model.NotFoundException
-import com.rumune.web.domain.cart.dto.CartDto
-import com.rumune.web.domain.cart.dto.CartProductDto
 import com.rumune.web.domain.cart.dto.request.AddProductToCartRequestProduct
 import com.rumune.web.domain.cart.entity.Cart
 import com.rumune.web.domain.cart.entity.CartProduct
@@ -23,29 +21,38 @@ class CartService(
     /**
      * 카트에 상품 담기
      */
-    fun addProductToCart(userId:Long, productList:List<AddProductToCartRequestProduct>):List<CartProduct> {
+    fun addProductToCart(userId: Long, productList: List<AddProductToCartRequestProduct>): List<CartProduct> {
         val productMap = productList.associateWith { it.count }.mapKeys { it.key.id }.toMutableMap()
         val cartOptional = cartRepository.findByUserId(userId)
         val cartProductEntityList = mutableListOf<CartProduct>()
         // 카트가 DB에 없으면 만들고 저장해서 카트에 상품을 담는다.
-        val cart:Cart = if(cartOptional.isEmpty) cartRepository.save(Cart(user = userRepository.findById(userId).get())) else cartOptional.get()
-        // 기존 상품 목록이 없다면 그냥 추가하고 있다면 개수를 수정한다.
-        if (cart.product.isNotEmpty()) {
-            cart.product.forEach {
-                it.count += productMap[it.product.id] ?: 0
-                cartProductEntityList.add(it)
-                productMap.remove(it.product.id)
+        val cart: Cart = when (cartOptional.isEmpty) {
+            true -> {
+                cartRepository.save(Cart(user = userRepository.findById(userId).get()))
+            }
+
+            false -> {
+                cartOptional.get()
             }
         }
+
+        // 기존 상품 목록이 없다면 그냥 추가하고 있다면 개수를 수정한다.
+        cart.product.forEach {
+            it.count += productMap[it.product.id] ?: 0
+            cartProductEntityList.add(it)
+            productMap.remove(it.product.id)
+        }
+
         // 기존 상품 목록에 더하고 새로 추가해야 할 것들은 전부 추가
-        productRepository.findProductList(productIdList = productMap.map{it.key}).forEach{ p ->
+        productRepository.findProductList(
+            productIdList = productMap.map { it.key }
+        ).forEach { p ->
             productMap[p.id]?.let {
                 cartProductEntityList.add(CartProduct(cart = cart, product = p, count = it))
             }
         }
-        val result = cartProductRepository.saveAll(cartProductEntityList)
 
-        return result
+        return cartProductRepository.saveAll(cartProductEntityList)
     }
 
     /**
